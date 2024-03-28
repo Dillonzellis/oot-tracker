c37b72f58f98bdb779574d43acb714c79ab180ee
dillonzellis <dillonzellis@gmail.com>
2024-03-28 00:15:04 -0400

    feat: map through items

--- db/schema.ts
+++ db/schema.ts
@@ -4,5 +4,5 @@ export const items = pgTable("items", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
-  active: boolean("active").notNull(),
+  active: boolean("active").notNull().default(false),
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import { Item } from "@/components/item";
-import dekuStick from "/public/deku-stick.png";
-import fairyOcarina from "/public/fairy-ocarina.png";
-import ocarinaTime from "/public/ocarina-time.png";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { getItems } from "../../db/queries";
 
@@ -14,13 +11,11 @@ export default async function Home() {
         <h1 className="scroll-m-20 pt-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
           OOT Tracker
         </h1>
-        {/* <div className="flex gap-x-4"> */}
-        {/*   <Item imgSrc={dekuStick} /> */}
-        {/*   <Item imgSrc={fairyOcarina} /> */}
-        {/*   <Item imgSrc={ocarinaTime} /> */}
-        <Item imgSrc={data[0].imageSrc} active={data[0].active} />
-        {/* </div> */}
-        {JSON.stringify(data[0].title)}
+        <div className="flex gap-x-4">
+          {data.map((item) => (
+            <Item imgSrc={item.imageSrc} active={item.active} />
+          ))}
+        </div>
       </MaxWidthWrapper>
     </main>
   );
