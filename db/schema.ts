afa9ac1e31aaf471e8439697f2b693ddb58703db
dillonzellis <dillonzellis@gmail.com>
2024-03-31 16:32:44 -0400

    feat: add tentative schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,15 @@
-import { pgTable, serial, boolean, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, boolean, text, integer } from "drizzle-orm/pg-core";
+
+export const userItems = pgTable("user_items", {
+  userid: serial("user_id").primaryKey(),
+  itemId: integer("item_id")
+    .references(() => items.id, { onDelete: "cascade" })
+    .notNull(),
+  itemState: boolean("item_state").notNull().default(false),
+});
 
 export const items = pgTable("items", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
-  active: boolean("active").notNull().default(false),
 });
