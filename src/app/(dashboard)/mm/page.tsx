20506dcbb0ade97207ee5923fe00cadf56a25fd3
dillonzellis <dillonzellis@gmail.com>
2024-04-01 14:44:11 -0400

    feat: add very rough mm page

---
+++ src/app/(dashboard)/mm/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+
+export default function MMHome() {
+  return (
+    <main className="">
+      <MaxWidthWrapper>
+        <h1>MM Tracker</h1>
+      </MaxWidthWrapper>
+    </main>
+  );
+}
