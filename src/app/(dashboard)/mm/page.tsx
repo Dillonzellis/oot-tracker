dca411ac249cbf51d492961858943ebf44bb391d
dillonzellis <dillonzellis@gmail.com>
2024-04-01 15:22:06 -0400

    feat: query mm items on mm root

--- src/app/(dashboard)/mm/page.tsx
+++ src/app/(dashboard)/mm/page.tsx
@@ -1,10 +1,21 @@
+import { Item } from "@/components/item";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { getItems } from "@/db/queries";
 
-export default function MMHome() {
+export default async function MMHome() {
+  const data = await getItems(2);
+
   return (
     <main className="">
       <MaxWidthWrapper>
-        <h1>MM Tracker</h1>
+        <h1 className="scroll-m-20 pt-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+          MM Tracker
+        </h1>
+        <div className="flex gap-x-4">
+          {data.map((item) => (
+            <Item key={item.itemId} item={item} />
+          ))}
+        </div>
       </MaxWidthWrapper>
     </main>
   );
