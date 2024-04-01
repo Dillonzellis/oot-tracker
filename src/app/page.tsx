dbbc3543be6132283314820bc27cfe51bc3f5abf
dillonzellis <dillonzellis@gmail.com>
2024-04-01 14:34:14 -0400

    refactor: mv db inside src dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Item } from "@/components/item";
 import MaxWidthWrapper from "@/components/max-width-wrapper";
-import { getItems } from "../../db/queries";
+import { getItems } from "@/db/queries";
 
 export default async function Home() {
   const data = await getItems();
