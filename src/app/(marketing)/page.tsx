ee9a55ef9b426d462784a625bb9b6d5f0764cb05
dillonzellis <dillonzellis@gmail.com>
2024-04-01 14:44:05 -0400

    style: style marketing page game picker very basic

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,10 +1,28 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
+import Link from "next/link";
+
+type GameLinkProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
+const GameLink = ({ href, children }: GameLinkProps) => {
+  return (
+    <Link href={href} className="text-xl font-medium">
+      {children}
+    </Link>
+  );
+};
 
 export default function Home() {
   return (
     <main className="">
       <MaxWidthWrapper>
-        <h1>Home</h1>
+        <h1 className="py-12 text-center text-2xl font-bold">Pick a game</h1>
+        <div className="flex justify-center gap-12">
+          <GameLink href="/oot">Ocarina of Time</GameLink>
+          <GameLink href="/mm">Majora's Mask</GameLink>
+        </div>
       </MaxWidthWrapper>
     </main>
   );
