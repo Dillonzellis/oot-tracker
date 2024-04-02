2bc21e10a94077d93e2afc225a3a7482840d1cd6
dillonzellis <dillonzellis@gmail.com>
2024-04-02 02:09:07 -0400

    refactor: make query titles more semantic

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -3,14 +3,41 @@ import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs";
 
 import db from "./drizzle";
-import { items, userItems } from "./schema";
+import { games, itemStates, items } from "./schema";
 
-export const getItems = cache(async (gameId: number) => {
+export const getGames = cache(async () => {
+  const data = await db.query.games.findMany();
+
+  return data;
+});
+
+export const getGamebyId = cache(async (gameId: number) => {
+  const data = await db.query.games.findFirst({
+    where: eq(games.gameId, gameId),
+  });
+
+  return data;
+});
+
+export const getItemsByGame = cache(async (gameId: number) => {
   const data = await db.query.items.findMany({
     where: eq(items.gameId, gameId),
   });
   return data;
 });
+
+// export const getItemsWithState = cache(async (userId: string) => {
+//   const data = await db.query.items.findMany({
+//     where: eq(items.gameId, itemId),
+//     with: {
+//       itemStates: {
+//         where: eq(itemStates.type, "FOUND"),
+//       },
+//     },
+//   });
+//
+//   return data;
+// });
 
 // export const getUserItems = cache(async () => {
 //   const { userId } = auth();
