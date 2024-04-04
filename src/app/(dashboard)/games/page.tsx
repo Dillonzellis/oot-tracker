378fa746ce16e3b95b2f64eaf9afe2c00be59b0a
dillonzellis <dillonzellis@gmail.com>
2024-04-04 14:35:28 -0400

    style: add userprogress message

--- src/app/(dashboard)/games/page.tsx
+++ src/app/(dashboard)/games/page.tsx
@@ -1,6 +1,11 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper";
 import { GameList } from "./game-list";
 import { getGames, getUserProgress } from "@/db/queries";
+import { ReactNode } from "react";
+
+const Heading = ({ children }: { children: ReactNode }) => {
+  return <h1 className="py-12 text-center text-2xl font-bold">{children}</h1>;
+};
 
 export default async function GamesPage() {
   const gamesData = await getGames();
@@ -14,7 +19,14 @@ export default async function GamesPage() {
   return (
     <main className="">
       <MaxWidthWrapper>
-        <h1 className="py-12 text-center text-2xl font-bold">Pick a game</h1>
+        {userProgress ? (
+          <Heading>
+            Hello, {userProgress.userName} pick up where you left off? or start
+            a new game?
+          </Heading>
+        ) : (
+          <Heading>Pick a game</Heading>
+        )}
         <GameList games={games} activeGameId={userProgress?.activeGameId} />
       </MaxWidthWrapper>
     </main>
