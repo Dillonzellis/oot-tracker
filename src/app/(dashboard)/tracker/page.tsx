a9d7be3ad988e968a76937a2e0cde3e24d7e0f95
dillonzellis <dillonzellis@gmail.com>
2024-04-11 14:33:08 -0400

    chore: remove testing code

--- src/app/(dashboard)/tracker/page.tsx
+++ src/app/(dashboard)/tracker/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
   const userData = await getUser();
   const gameItemsData = await getItemsByActiveGame();
   const userItemsData = await getItemsByUserWithState();
-  // const userGames = await getUserGames();
 
   const [user, userItems, gameItems] = await Promise.all([
     userData,
@@ -27,8 +26,6 @@ export default async function Home() {
   return user.activeGameId ? (
     <main className="">
       <MaxWidthWrapper>
-        {/* {JSON.stringify(userGames)} */}
-        {JSON.stringify(userItems.map((item) => item.id + item.state))}
         <h1 className="scroll-m-20 pt-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
           {user.activeGameId}
         </h1>
