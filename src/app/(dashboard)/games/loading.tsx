68cbf39e7a047c4daf66c1ce1e9e4d369a4b2e5b
dillonzellis <dillonzellis@gmail.com>
2024-04-11 14:26:52 -0400

    stlye: fix height on loading pages

--- src/app/(dashboard)/games/loading.tsx
+++ src/app/(dashboard)/games/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex min-h-[calc(100vh-57px)] w-full items-center justify-center">
       <Loader className="h-6 w-6 animate-spin text-muted-foreground" />
     </div>
   );
